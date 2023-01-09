979cfadd13f21ccde7330f94f8a6af5795fc6c66
Florian Rupprecht <floruppr@gmail.com>
2023-01-09 21:11:34 +0100

    Updates

--- _app/immutable/chunks/2-7b854c0f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-6ba86a1a.js";export{t as component};
---
+++ _app/immutable/chunks/2-d13169d8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-812598c3.js";export{t as component};
