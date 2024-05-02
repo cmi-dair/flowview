d9dd14f17aa5e77fcc90bba09d13cf7e6343d844
Florian Rupprecht <floruppr@gmail.com>
2024-05-02 09:51:15 -0400

    Dont scroll in adjacency matrix card

--- src/lib/InspectCard.svelte
+++ src/lib/InspectCard.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     export let title = '';
+
+    // if true, the height of the card will be determined by the content
     export let free_height: boolean = false;
     export let basis: string = 'basis-1/2';
 </script>
 
-<div class="min-h-40 rounded-md border shadow-md p-2 m-2 {basis} grow-0 overflow-auto">
+<div class="rounded-md border shadow-md p-2 m-2 {basis}">
     <h2 class="font-medium mb-1">{title}</h2>
-    <div class="{free_height ? '': 'max-h-80 overflow-auto'}">
+    <div class="{free_height ? '': 'min-h-40 max-h-80 overflow-auto grow-0 overflow-auto'}">
         <slot/>
     </div>
 </div>
